6d593059292747962a3bbbf94b105e55ed55ad39
C00LSkY <c00lspam1@gmail.com>
2019-03-07 17:01:50 +0300

    добавлен тест для авторизации в админ панель

---
+++ tests/authorization.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    request.addfinalizer(wd.quit)
+    return wd
+
+def test_login(driver):
+    driver.get("http://litecart/admin/login.php")
+    wait = WebDriverWait(driver, 10) # seconds
+    driver.find_element_by_xpath("//input[@name='username']").send_keys("admin")
+    driver.find_element_by_xpath("//input[@name='password']").send_keys("admin")
+    driver.find_element_by_xpath("//button[@name='login']").click()
+    wait.until(EC.title_is("My Store"))
