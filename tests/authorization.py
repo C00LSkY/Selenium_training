bffe168e1ef602063b8d24af41f20a3747a38009
C00LSkY <c00lspam1@gmail.com>
2019-03-11 23:00:10 +0300

    добавлне вариант запуска FireFox  по старой счеме

--- tests/authorization.py
+++ tests/authorization.py
@@ -8,15 +8,17 @@ import os
 @pytest.fixture
 def driver(request):
     # wd = webdriver.Ie(capabilities={"requireWindowFocus": True})
-    wd = webdriver.Firefox(capabilities={"marionette": True})
+    # wd = webdriver.Firefox(capabilities={"marionette": True})
+    wd = webdriver.Firefox(capabilities={"marionette": False}, firefox_binary={"c:\\Program Files (x86)\\Nightly\\firefox.exe"})
     # wd = webdriver.Chrome(capabilities={"unexpectedAlertBehaviour": "dismiss"})
     print(wd.capabilities)
     request.addfinalizer(wd.quit)
     return wd
 
+
 def test_login(driver):
     driver.get("http://litecart/admin/login.php")
-    wait = WebDriverWait(driver, 10) # seconds
+    wait = WebDriverWait(driver, 10)  # seconds
     driver.find_element_by_xpath("//input[@name='username']").send_keys("admin")
     driver.find_element_by_xpath("//input[@name='password']").send_keys("admin")
     driver.find_element_by_xpath("//button[@name='login']").click()
